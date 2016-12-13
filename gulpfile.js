357e7cb58986d0ba802c704f0c7d86e56ed6d499
Paul Esch-Laurent <herppfel@gmail.com>
2016-12-13 02:46:57 -0500

    Setup Gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp        = require("gulp"),
+    plugins     = require("gulp-load-plugins")(),
+    browserSync = require("browser-sync").create(),
+    pkg         = require("./package.json"),
+    config = {
+      autoprefixer: {
+        browsers: ["last 2 versions"]
+      },
+      browserSync: {
+        server: {
+          baseDir: "./"
+        },
+        notify: false
+      }
+    };
+
+gulp.task("browser-sync", function() {
+  browserSync.init(config.browserSync);
+});
+
+gulp.task("build:css", function() {
+  return gulp.src("scss/*.scss")
+    .pipe(plugins.plumber())
+    .pipe(plugins.sass.sync())
+    .pipe(plugins.autoprefixer(config.autoprefixer))
+    .pipe(gulp.dest("css/"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("serve", ["browser-sync"], function() {
+  gulp.watch("scss/**/*.scss", ["build:css"]);
+  gulp.watch("*.html", browserSync.reload)
+});
