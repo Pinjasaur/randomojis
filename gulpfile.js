63915b1051a2c62aaaf8a8218b3e6416eb92f68d
Paul Esch-Laurent <herppfel@gmail.com>
2017-02-10 23:52:59 -0500

    Add JS linting/reloading to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,19 @@ var gulp        = require("gulp"),
         server: {
           baseDir: "./"
         },
-        notify: false
+        notify: false,
+        // Create a tunnel (if using `--tunnel`) with a subdomain of:
+        // 1. the first "chunk" of the package.json `name`
+        // 2. a random 6-character string appended to it
+        // Note: needs to be lowercased alphanumerics
+        tunnel: plugins.util.env.tunnel ?
+                (pkg.name.trim().toLowerCase().split(/[^a-zA-Z0-9]/g)[0] + // [1]
+                Math.random().toString(36).substr(2, 6)) :                 // [2]
+                false,
       }
     };
 
-gulp.task("browser-sync", function() {
-  browserSync.init(config.browserSync);
-});
-
+// Build the SCSS (Autoprefixer, Sourcemaps)
 gulp.task("build:css", function() {
   return gulp.src("scss/*.scss")
     .pipe(plugins.plumber())
@@ -27,8 +32,18 @@ gulp.task("build:css", function() {
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task("serve", ["browser-sync"], function() {
+// Build the JS (lint)
+gulp.task("build:js", function() {
+  // Ignore JS in the js/vendor directory since we didn't author it
+  return gulp.src([paths.src + "/js/**/*.js", "!" + paths.src + "/js/{vendor,vendor/**}"])
+    .pipe(plugins.jshint(config.jshint))
+    .pipe(plugins.jshint.reporter("jshint-stylish"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("serve", function() {
+  browserSync.init(config.browserSync);
   gulp.watch("scss/**/*.scss", ["build:css"]);
-  gulp.watch("js/**/*.js", browserSync.reload);
+  gulp.watch("js/**/*.js", ["build:js"]);
   gulp.watch("*.html", browserSync.reload)
 });
